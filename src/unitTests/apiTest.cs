dd472801597035981ae39b2f00cd2c5544f7f7e8
Alex Servirog <servalex1@gmail.com>
2020-04-05 15:01:20 +0300

    moved api project to separate directory, added solution and empty test project

---
+++ src/unitTests/apiTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace unitTests
+{
+    public class apiTest
+    {
+        [Fact]
+        public void rootTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
