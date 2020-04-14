6642b30864c9ae9c924ed1fe103781df477257be
CorvusChrome <rentinchibs@gmail.com>
2020-04-14 13:43:16 +0300

    EZ-23 Code fixed according to comments above. Toggle removed, link changed from 'open' to 'all' issue, sort files into folders, add individual css for items. Minor style changes

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,51 +1,40 @@
 import React, { useState } from 'react';
-import TodoList from './Todo/TodoList'
-import Context from './context'
-import AddTodo from './Todo/AddTodo';
+import TodoList from './Todo/TodoList/TodoList';
+import AddTodo from './Todo/AddTodo/AddTodo';
 
 function App() {
-  const [todos, setTodos] = React.useState([  ])
+  const [todos, setTodos] = useState([]);
 
   useState(() => {
-    fetch('https://api.github.com/repos/randomdima/ez/issues')
+    fetch('https://api.github.com/repos/randomdima/ez/issues?state=all')
       .then(response => response.json())
-      .then(todos => {
-        setTodos(todos)
-      })
-  }, [])
+      .then(setTodos);
+  })
+  console.log(todos)
 
-  function toggoleTodo(id) {
-    setTodos(todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    }))
-  }
+
+
   function addTodo(title) {
-    setTodos(todos.concat([{
+    setTodos(todos.concat({
       title,
       id: Date.now(),
       completed: false
-    }]))
+    }));
   }
 
   function removeTodo(id) {
-    setTodos(todos.filter(todo => todo.id !== id))
+    setTodos(todos.filter(todo => todo.id !== id));
   }
 
   return (
-    <Context.Provider value={{ removeTodo }}>
-      <div>
-        <div className="header">
-          <b> Some ugly react </b>
-        </div>
-        <AddTodo onCreate={addTodo} />
-        {todos.length ? <TodoList todos={todos} onToggle={toggoleTodo}></TodoList> :
-          <p>No todos</p>}
-
+    <div>
+      <div className="header">
+        <b> EZ all issues </b>
       </div>
-    </Context.Provider>
+      <AddTodo onCreate={addTodo} />
+      {todos.length ? <TodoList removeTodo={removeTodo} todos={todos}></TodoList> :
+        <p>No todos</p>}
+    </div>
   )
 }
 
---
+++ webapp/src/Todo/TodoList/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import TodoItem from '../TodoItem/TodoItem'
+import PropTypes from 'prop-types'
+import './TodoList.css'
+
+// function TodoList(prop) {
+//     return (
+//         <ul>
+//             {prop.todos.map((todo, index) => <TodoItem todo={todo} key={todo.id} index={index} />)}
+//         </ul>
+//     )
+// }
+function TodoList({todos,removeTodo}) {    
+    return (
+        <ul>
+            {todos.map((todo, index) => <TodoItem todo={todo} key={todo.id} index={index} removeTodo={removeTodo} />)}
+        </ul>
+    )
+}
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    removeTodo: PropTypes.func.isRequired
+}
+
+export default TodoList
