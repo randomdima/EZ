74876aa354576d58e8824b3d0342f8454e7f7790
RandoM RandoM <dmytroz@tradeix.com>
2020-04-21 14:05:09 +0300

    cors

--- api/EZ.Api/Startup.cs
+++ api/EZ.Api/Startup.cs
@@ -14,7 +14,13 @@ namespace EZ.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddCors();
+            services.AddCors(c =>
+            {
+                c.AddDefaultPolicy(q =>
+                    q.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
