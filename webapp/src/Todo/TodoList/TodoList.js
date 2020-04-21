61bf1a9524b8ad455a434bdd088c06c6df90c018
CorvusChrome <rentinchibs@gmail.com>
2020-04-21 18:00:36 +0300

    Adjusting UI for DB

--- webapp/src/Todo/TodoList/TodoList.js
+++ webapp/src/Todo/TodoList/TodoList.js
@@ -3,13 +3,18 @@ import TodoItem from '../TodoItem/TodoItem'
 import PropTypes from 'prop-types'
 import './TodoList.css'
 
-function TodoList({todos,removeTodo}) {    
+function TodoList({ todos, removeTodo }) {
     return (
         <ul>
-            {todos.map((todo, index) => <TodoItem todo={todo} key={todo.id} index={index} removeTodo={removeTodo} />)}
+            {todos.map((todo, index) => <TodoItem
+                todo={todo}
+                key={todo.id}
+                index={index}
+                removeTodo={removeTodo} />)}
         </ul>
     )
 }
+
 TodoList.propTypes = {
     todos: PropTypes.arrayOf(PropTypes.object).isRequired,
     removeTodo: PropTypes.func.isRequired
